c4d674ffd03ff3570ca0d11aaf6cde4b4c79c56a
qiqiting <133198413+qiqiting@users.noreply.github.com>
2023-03-11 12:25:45 +0000

    Updating readme file.

--- repos/model_repo.py
+++ repos/model_repo.py
@@ -6,3 +6,9 @@ import re
 
 import torch
 from model import ExLlama, ExLlamaCache, ExLlamaConfig
+from tokenizer import ExLlamaTokenizer
+from generator import ExLlamaGenerator
+
+# Used in json if &{key} exists in a value it should be replaced with a value from the main json dict such as userName
+mainDictTarget = "&"
+# Used in json if @{key} exists in a value it should be replaced with a value from the character json dict such as charName
