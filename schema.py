0c8583db55ab6587f70716c047484128e23f9e58
qiqiting <133198413+qiqiting@users.noreply.github.com>
2023-03-28 22:34:50 +0000

    num

--- schema.py
+++ schema.py
@@ -1,3 +1,9 @@
 
 chatSchema = {
     "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "userName": {
+            "type": "string"
+        },
+        "genParams": {
