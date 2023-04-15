f86f3cd476c2a2843391a02be60b99ff5ce8cc45
qiqiting <133198413+qiqiting@users.noreply.github.com>
2023-04-15 07:27:49 +0000

    Fixes from review

--- server.py
+++ server.py
@@ -8,3 +8,15 @@ from repos.model_repo import LlamaModel, LlamaModelRepo
 from flask_expects_json import expects_json
 import schema
 app = Flask(__name__)
+
+modelRepo = LlamaModelRepo()
+llamaModel = None
+model = None
+
+
+emotionsPromptTemplate = {
+    "firstUser": "User1",
+    "secondUser": "User2",
+    "genParams": {
+        "max_length": 1000,
+        "top_p": 0.7,
