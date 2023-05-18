215b481ffda5b15e651f5efe042f191f54061a95
qiqiting <133198413+qiqiting@users.noreply.github.com>
2023-05-18 18:23:35 +0000

    3.2.2

--- server.py
+++ server.py
@@ -172,3 +172,12 @@ def chat():
 
     print("Returning ===============================================")
     print(responseDict)
+    print("==================================================================")
+    return responseDict
+
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True)
